7368a7820eff7c9f10854846faad5c028498280b
manigandan <ksmani07@gmail.com>
2017-04-20 23:49:55 +0530

    initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,14 @@
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.config.common.js');
+var path = require('path');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: path.resolve(__dirname, 'public/js/app'),
+        publicPath: "/js/app/",
+        filename: 'bundle.js',
+        chunkFilename: '[id].chunk.js'
+    }
+});
